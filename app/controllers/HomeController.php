4fc041ac307de9f974d7042c8172cb82251c944e
Gavin <gavinzhou10c@gmail.com>
2014-02-01 23:53:44 -0500

    Add doLogin() method which validates email address, and password format. The password format has to be alphanumeric.

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -22,7 +22,45 @@ class HomeController extends BaseController {
 		return View::make('login');
 	}
 	
-	public function doLogin(){
-		// process the form
-	}
-}
+	public function doLogin()
+	{
+		// validate the info, create rules for the inputs
+		$rules = array(
+			'email'    => 'required|email', // make sure the email is an actual email
+			'password' => 'required|alphaNum|min:3' // password can only be alphanumeric and has to be greater than 3 characters
+		);
+
+		// run the validation rules on the inputs from the form
+		$validator = Validator::make(Input::all(), $rules);
+
+		// if the validator fails, redirect back to the form
+		if ($validator->fails()) {
+			return Redirect::to('login')
+				->withErrors($validator) // send back all errors to the login form
+				->withInput(Input::except('password')); // send back the input (not the password) so that we can repopulate the form
+		} else {
+
+			// create our user data for the authentication
+			$userdata = array(
+				'email' 	=> Input::get('email'),
+				'password' 	=> Input::get('password')
+			);
+
+			// attempt to do the login
+			if (Auth::attempt($userdata)) {
+
+				// validation successful!
+				// redirect them to the secure section or whatever
+				// return Redirect::to('secure');
+				// for now we'll just echo success (even though echoing in a controller is bad)
+				echo 'SUCCESS!';
+
+			} else {	 	
+
+				// validation not successful, send back to form	
+				return Redirect::to('login');
+
+			}
+
+		}
+	}}
