63639b5e0905d88b54b1ee23ee34ca7d4b0e9c30
google.GavinZhou <google.gavinzhou10c@gmail.com>
2014-02-02 16:15:46 -0500

    Create register and able to record the username and password in the database.

--- app/models/User.php
+++ app/models/User.php
@@ -48,5 +48,13 @@ class User extends Eloquent implements UserInterface, RemindableInterface {
 	{
 		return $this->email;
 	}
+	
+	public static $rules = array(
+		'firstname'=>'required|alpha|min:2',
+		'lastname'=>'required|alpha|min:2',
+		'email'=>'required|email|unique:users',
+		'password'=>'required|alpha_num|between:6,12|confirmed',
+		'password_confirmation'=>'required|alpha_num|between:6,12'
+   );
 
 }
