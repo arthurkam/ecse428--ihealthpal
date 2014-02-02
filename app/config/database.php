a5d6a4ec9830efcf2a2c31e3334ec0eadba5de92
Gavin <gavinzhou10c@gmail.com>
2014-02-01 21:48:07 -0500

    Created user-create-table migration.

--- app/config/database.php
+++ app/config/database.php
@@ -55,9 +55,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'database',
+			'database'  => 'iHealthPal',
 			'username'  => 'root',
-			'password'  => '',
+			'password'  => 'ecse428',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
