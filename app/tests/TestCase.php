25ec33fa902c4af51458bc2fd159d7b379adb237
YangZhou91 <yang.zhou4@mail.mcgill.ca>
2014-02-04 16:41:58 -0500

    Add migrate test to the TestCase.

--- app/tests/TestCase.php
+++ app/tests/TestCase.php
@@ -1,19 +1,35 @@
 <?php
-
 class TestCase extends Illuminate\Foundation\Testing\TestCase {
-
-	/**
-	 * Creates the application.
-	 *
-	 * @return \Symfony\Component\HttpKernel\HttpKernelInterface
-	 */
-	public function createApplication()
-	{
-		$unitTesting = true;
-
-		$testEnvironment = 'testing';
-
-		return require __DIR__.'/../../bootstrap/start.php';
-	}
-
+  
+  /**
+   * Default preparation for each test
+   */
+  public function setUp()
+  {
+    parent::setUp();
+  
+    $this->prepareForTests();
+  }
+  
+  /**
+   * Creates the application.
+   *
+   * @return Symfony\Component\HttpKernel\HttpKernelInterface
+   */
+  public function createApplication()
+  {
+    $unitTesting = true;
+  
+    $testEnvironment = 'testing';
+  
+    return require __DIR__.'/../../start.php';
+  }
+  
+  /**
+   * Migrate the database
+   */
+  private function prepareForTests()
+  {
+    Artisan::call('migrate');
+  }
 }
