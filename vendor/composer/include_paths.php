9895aa1309cf5346050f43ce66da8b3861422c7c
www-data <www-data@ip-172-31-33-64.us-west-2.compute.internal>
2014-02-04 05:07:23 +0000

    Merge due to upgrade of phpunit.

--- vendor/composer/include_paths.php
+++ vendor/composer/include_paths.php
@@ -6,7 +6,6 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
-    $vendorDir . '/phpseclib/phpseclib/phpseclib',
     $vendorDir . '/phpunit/php-text-template',
     $vendorDir . '/phpunit/phpunit-mock-objects',
     $vendorDir . '/phpunit/php-timer',
@@ -15,4 +14,5 @@ return array(
     $vendorDir . '/phpunit/php-code-coverage',
     $vendorDir . '/phpunit/phpunit',
     $vendorDir . '/symfony/yaml',
+    $vendorDir . '/phpseclib/phpseclib/phpseclib',
 );
