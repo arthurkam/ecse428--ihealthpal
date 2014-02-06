f76d91d25692af72d2f38244ae23b8f61c384eb1
YangZhou91 <yang.zhou4@mail.mcgill.ca>
2014-02-06 16:16:48 -0500

    Fix the broke link of home page.

--- app/views/index.php
+++ app/views/index.php
@@ -37,7 +37,7 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand" href="index.html">iHealthPal</a>
+          <a class="navbar-brand" href="/index">iHealthPal</a>
           <a class="navbar-brand" href="about.html">About</a>
           <a class="navbar-brand" href="contact.html">Contact</a>
         </div>
