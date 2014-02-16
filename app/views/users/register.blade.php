ae98792d5ee5e7d3d7210bf7a1b59655ebc17112
Yang Zhou <gavinzhou10c@gmail.com>
2014-02-15 20:07:09 -0500

    Add logo to the register page.

--- app/views/users/register.blade.php
+++ app/views/users/register.blade.php
@@ -8,13 +8,13 @@
     <meta name="author" content="">
     <link rel="shortcut icon" href="../../assets/ico/favicon.ico">
 
-    <title>iHealthPal | Register</title>
+    <title>iHealthPal</title>
 
     <!-- Bootstrap core CSS -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="/css/bootstrap.min.css" rel="stylesheet">
 
     <!-- Custom styles for this template -->
-    <link href="css/jumbotron.css" rel="stylesheet">
+    <link href="/css/jumbotron.css" rel="stylesheet">
 
     <!-- Just for debugging purposes. Don't actually copy this line! -->
     <!--[if lt IE 9]><script src="js/ie8-responsive-file-warning.js"></script><![endif]-->
@@ -24,6 +24,15 @@
       <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
       <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
     <![endif]-->
+    <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+      ga('create', 'UA-47850364-1', 'inc.gs');
+      ga('send', 'pageview');
+    </script>
   </head>
 
   <body>
@@ -38,22 +47,31 @@
             <span class="icon-bar"></span>
           </button>
           <a class="navbar-brand" href="/">iHealthPal</a>
-          <a class="navbar-brand" href="about">About</a>
-          <a class="navbar-brand" href="contact">Contact</a>
+          <a class="navbar-brand" href="/about">About</a>
+          <a class="navbar-brand" href="/contact">Contact</a>
         </div>
         <div class="navbar-collapse collapse">
-          <form class="navbar-form navbar-right" role="form" method="post">
+          <form action="users/login" class="navbar-form navbar-right" role="form" method="post">
             <div class="form-group">
-              <input type="text" placeholder="Email" class="form-control">
+              <input name="email" type="text" placeholder="Email" class="form-control">
             </div>
             <div class="form-group">
-              <input type="password" placeholder="Password" class="form-control">
+              <input name="password" type="password" placeholder="Password" class="form-control">
             </div>
-            <button type="submit" class="btn btn-success">Sign in</button>
+            <button type="submit" class="btn btn-success">
+              <span class="glyphicon glyphicon-log-in"></span> &nbsp;Login
+            </button>
           </form>
         </div><!--/.navbar-collapse -->
       </div>
     </div>
+
+    <!-- Main jumbotron for a primary marketing message or call to action -->
+    <div class="jumbotron">
+      <div class="container">
+        <img src="/img/logo.png" alt="logo" height="200">
+	<p>Health and Fitness Tracking System.</p>
+
     
     <!-- Opening a form, by defalut, the Post method is assumed. -->
 	{{ Form::open(array('url'=>'users/create', 'class'=>'form-signup')) }}
@@ -64,21 +82,22 @@
 	         <li>{{ $error }}</li>
 	      @endforeach
 	   </ul>
-	   		{{Form::label('firstname', 'First Name :		')}}
+	   		{{Form::label('firstname', 'First Name :')}}
 	   		{{ Form::text('firstname', null, array('class'=>'input-block-level', 'placeholder'=>'First Name')) }}
 		   </br>
-		   {{Form::label('lastname', 'Last Name :		')}}
+		   {{Form::label('lastname', 'Last Name :')}}
 		   {{ Form::text('lastname', null, array('class'=>'input-block-level', 'placeholder'=>'Last Name')) }}
 		   </br>
-		   {{Form::label('email', 'E-mail Address :		')}}
+		   {{Form::label('email', 'E-mail Address :')}}
 	       {{ Form::text('email', null, array('class'=>'input-block-level', 'placeholder'=>'Email Address')) }}
 		   </br>
-		   {{Form::label('password', 'Password :		')}}
+		   {{Form::label('password', 'Password :')}}
 	       {{ Form::password('password', array('class'=>'input-block-level', 'placeholder'=>'Password')) }}
-		   {{Form::label('password_confirmation', 'Confirm Password :		')}}
+		   </br>
+		   {{Form::label('password_confirmation', 'Confirm Password :')}}
 		   {{ Form::password('password_confirmation', array('class'=>'input-block-level', 'placeholder'=>'Confirm Password')) }}
-		 </br>
-		   {{ Form::submit('Register', array('class'=>'btn btn-large btn-primary btn-block'))}}
+		   </br>
+		   {{ Form::submit('Register', array('class'=>'btn btn-large btn-primary'))}}
 	   
 	{{ Form::close() }}
 
