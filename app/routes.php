25f679c388b7b74fa635e6b7a2603372f2c2e4ef
Yang Zhou <gavinzhou10c@gmail.com>
2014-02-16 01:15:23 -0500

    The firstname, lastname, email address are able to wrtie to the usrs table.

--- app/routes.php
+++ app/routes.php
@@ -27,15 +27,53 @@
 	Route::post('users/login', array('uses' => 'HomeController@doLogin'));
 	
 	// Route to facebook login
-	Route::get('login/fb', function(){
+	Route::get('login/facebook', function(){
 		$facebook = new Facebook(Config::get('facebook'));	
 		$params = array(
-			'redirect_uri'=>url('/login/fb/callback'),
+			'redirect_uri'=>url('/login/facebook/callback'),
 			'scope'=>'email',
 		);
 		return Redirect::to($facebook->getLoginUrl($params));
 	});
 	
+	// Route for facebook callback
+	Route::get('login/facebook/callback', function() {
+	    $code = Input::get('code');
+	    if (strlen($code) == 0) return Redirect::to('/')->with('message', 'There was an error communicating with Facebook');
+	 
+	    $facebook = new Facebook(Config::get('facebook'));
+	    $uid = $facebook->getUser();
+	 
+	    if ($uid == 0) return Redirect::to('/')->with('message', 'There was an error');
+	 
+	    $me = $facebook->api('/me');
+	 
+	    $profile = Profile::whereUid($uid)->first();
+	    if (empty($profile)) {
+	 
+	        $user = new User;
+	        $user->firstname = $me['first_name'];
+	        $user->lastname = $me['last_name'];
+	        $user->email = $me['email'];
+	        //$user->photo = 'https://graph.facebook.com/'.$me['username'].'/picture?type=large';
+	 
+	        $user->save();
+	 
+	        $profile = new Profile();
+	        $profile->uid = $uid;
+	        $profile->username = $me['username'];
+	        $profile = $user->profiles()->save($profile);
+	    }
+	 
+	    $profile->access_token = $facebook->getAccessToken();
+	    $profile->save();
+	 
+	    $user = $profile->user;
+	 
+	    Auth::login($user);
+	 
+	    return Redirect::to('/')->with('message', 'Logged in with Facebook');
+});	
 	// Route to the home page, after login
 	Route::any('home', array('uses' => 'UsersController@getDashboard'));
 	
