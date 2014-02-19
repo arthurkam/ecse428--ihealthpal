54ded566b36a07cd94f39d5a819a53863452745d
Yang Zhou <gavinzhou10c@gmail.com>
2014-02-18 23:11:32 -0500

    Clean up facebook callback route in the routes.php.

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -102,6 +102,46 @@ class HomeController extends BaseController {
 		);
 		return Redirect::to($facebook->getLoginUrl($params));
 	}
+	
+	public function callbackFacebook()
+	{
+	    $code = Input::get('code');
+	    if (strlen($code) == 0) return Redirect::to('/')->with('message', 'There was an error communicating with Facebook');
+	 
+	    $facebook = new Facebook(Config::get('facebook'));
+	    $uid = $facebook->getUser();
+	 
+	    if ($uid == 0) return Redirect::to('/')->with('message', 'There was an error');
+	 
+	    $me = $facebook->api('/me');
+	 
+	    $profile = Profile::whereUid($uid)->first();
+	    if (empty($profile)) {
+	 
+	        $user = new User;
+	        $user->firstname = $me['first_name'];
+	        $user->lastname = $me['last_name'];
+	        $user->email = $me['email'];
+	        //$user->photo = 'https://graph.facebook.com/'.$me['username'].'/picture?type=large';
+	 
+	        $user->save();
+	 
+	        $profile = new Profile();
+	        $profile->uid = $uid;
+	        $profile->username = $me['username'];
+	        $profile = $user->profiles()->save($profile);
+	    }
+	 
+	    $profile->access_token = $facebook->getAccessToken();
+	    $profile->save();
+	 
+	    $user = $profile->user;
+	 
+	    Auth::login($user);
+	 
+	    return Redirect::to('/')->with('message', 'Logged in with Facebook');
+		}
+	
 	public function doLogout()
 	{
 		Session::flush();
