6638dec35a0d9606650937620cb35d1468478506
Yang Zhou <gavinzhou10c@gmail.com>
2014-02-19 12:06:10 -0500

    Use main layout to decorate reset password page.

--- app/views/password/reset.blade.php
+++ app/views/password/reset.blade.php
@@ -1,10 +1,13 @@
-<form action="{{ action('RemindersController@postReset') }}" method="POST">
-    <input type="hidden" name="token" value="{{ $token }}">
-    Email:<input type="email" name="email">
-    </br>
-    Password:<input type="password" name="password">
-    </br>
-    Password Confirm:<input type="password" name="password_confirmation">
-    </br>
-    <input type="submit" value="Reset Password">
-</form>
+@extends('layouts.main')
+@section('content')	
+	<form action="{{ action('RemindersController@postReset') }}" method="POST">
+	    <input type="hidden" name="token" value="{{ $token }}">
+	    Email:<input type="email" name="email">
+	    </br>
+	    Password:<input type="password" name="password">
+	    </br>
+	    Password Confirm:<input type="password" name="password_confirmation">
+	    </br>
+	    <input type="submit" value="Reset Password">
+	</form>
+@stop
