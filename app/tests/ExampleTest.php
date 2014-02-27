3efa25a41da268f7aff442bdbc857171965d8f07
Yang Zhou <gavinzhou10c@gmail.com>
2014-02-27 17:21:26 -0500

    Create UsernameIsRequired test case for an example.

--- app/tests/ExampleTest.php
+++ app/tests/ExampleTest.php
@@ -12,6 +12,28 @@ class ExampleTest extends TestCase {
 		$crawler = $this->client->request('GET', '/');
 
 		$this->assertTrue($this->client->getResponse()->isOk());
+		
 	}
 
+	public function testUsernameIsReqired()
+	{
+		//Create a new User
+		$user = new User;
+		$user->email = "unittest@unittest.com";
+		$user->password = "password";
+		$user->password_confirmation = "password";
+		
+		
+		  // User should not save
+		  $this->assertFalse($user->save());
+		 
+		  // Save the errors
+		  $errors = $user->errors()->all();
+		 
+		  // There should be 1 error
+		  $this->assertCount(1, $errors);
+		 
+		  // The username error should be set
+		  $this->assertEquals($errors[0], "The username field is required.");
+	}
 }
--- app/tests/TestCase.php
+++ app/tests/TestCase.php
@@ -1,7 +1,18 @@
 <?php
 
 class TestCase extends Illuminate\Foundation\Testing\TestCase {
-
+	
+	
+  /**
+   * Default preparation for each test
+   */
+	public function setUp()
+	  {
+	    parent::setUp();
+	  
+	    $this->prepareForTests();
+	  }
+  	
 	/**
 	 * Creates the application.
 	 *
@@ -15,5 +26,12 @@ class TestCase extends Illuminate\Foundation\Testing\TestCase {
 
 		return require __DIR__.'/../../bootstrap/start.php';
 	}
-
+	
+	/**
+	* Migrate the database
+	*/
+	private function prepareForTests()
+	  {
+	    Artisan::call('migrate');
+	  }
 }
