f26f9011c423b73788888a68a54253b3ed56e9f4
anitaszilagyi <anita.szilagyi@mail.mcgill.ca>
2014-02-28 00:15:39 -0500

    Resources

--- app/views/resource.blade.php
+++ app/views/resource.blade.php
@@ -64,8 +64,27 @@
     <div class="jumbotron">
       <div class="container">
         <img src="img/logo.png" alt="logo" height="100">
+        
+        	<p>Health and Fitness Tracking System.</p>
+
+    <div class="container" id="body">
+      <div class="row">
+        <h2>Resources</h2>
+         <div style="height: 500px;">
+            <p><b>Here are some resources that might interest you:</b></p>
+			<p><a href="http://www.reddit.com/r/loseit/wiki/faq">Reddit's Weight Loss Forum FAQ</a></p>
+			<p><a href="http://www.livestrong.com">Weight loss tips from LiveStrong.com</a></p>
+			<p><a href="http://www.wisegeek.com/what-does-200-calories-look-like.htm">What 200 calories look like</a></p>
+			<p><a href="http://www.huffingtonpost.ca/2013/10/18/gain-weight-_n_3957267.html">HuffPost's tips on healthy weight-gain</a></p>
+			<p><a href="http://www.webmd.com/diet/features/5-weight-loss-myths">Myths about weight loss</a></p>
+			<p><a href="http://ngm.nationalgeographic.com/2013/08/sugar/cohen-text">Truths about sugar in foods</a></p>			
+			<p><a href="http://www.helpguide.org/life/healthy_diet_fats.htm">Not all fats are bad for you!</a></p>			
+			<p><a href="http://www.huffingtonpost.com/2012/07/31/i-lost-weight-christy-caby_n_1724498.html">A success story: How counting calories helped weight loss</a></p>	
+			<p><a href="http://www.webmd.com/diet/features/overcoming-overeating">How to stop overeating</a></p>							
+			<p><a href="http://www.heartandstroke.com/site/c.ikIQLcMWJtE/b.3484019/k.6437/HeartHealthy_Recipes.htm">Hearty receipes</a></p>			
+         </div>
       </div>
-    </div>
+
 
   
 
