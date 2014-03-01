3fd133fb87410e57ca19947f156f815e8e7a9605
Yang Zhou <gavinzhou10c@gmail.com>
2014-02-28 21:02:41 -0500

    Use main layout to modify the index page.

--- app/views/layouts/main.blade.php
+++ app/views/layouts/main.blade.php
@@ -47,8 +47,11 @@
             <span class="icon-bar"></span>
           </button>
           <a class="navbar-brand" href="/">iHealthPal</a>
-          <a class="navbar-brand" href="/about">About</a>
-          <a class="navbar-brand" href="/contact">Contact</a>
+          <a class="navbar-brand" href="about">About</a>
+          <a class="navbar-brand" href="contact">Contact</a>
+          <a class="navbar-brand" href="resource">Resource</a>
+          <a class="navbar-brand" href="help">Help&nbsp;<span class="glyphicon glyphicon-question-sign"></span></a>         
+>>>>>>> blank_page_fix
         </div>
         <div class="navbar-collapse collapse">
           <form action="users/login" class="navbar-form navbar-right" role="form" method="post">
@@ -61,6 +64,7 @@
             <button type="submit" class="btn btn-success">
               <span class="glyphicon glyphicon-log-in"></span> &nbsp;Login
             </button>
+            <a class="btn btn-warning" href="password/remind" role="button">Forgot Password?</a>
           </form>
         </div><!--/.navbar-collapse -->
       </div>
