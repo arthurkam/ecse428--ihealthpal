a00c7260bd05a18c6219d39b86b313212f4aac23
Yang Zhou <gavinzhou10c@gmail.com>
2014-03-03 13:48:54 -0500

    Change the style of text field and button of the goal page.

--- app/views/users/goals.blade.php
+++ app/views/users/goals.blade.php
@@ -4,13 +4,17 @@
 	{{ Form::open(array('url' => 'goals/set')) }}
     	{{Form::label('Goals', 'Choose your goals: ')}}   
     	{{Form::select('goal_type', array('L' => 'Lose weight', 'G' => 'Gain weight'));}}
-    	{{Form::text('weight', '10');}}
+    	<br>
+    	{{Form::text('weight', '10', array('class' => 'form-control'));}}
     	{{Form::label('weight_unit', 'kg');}}
+    	<br>
     	{{Form::label('over', 'over');}}
-    	{{Form::text('time_interval', '12');}}
+    	{{Form::text('time_interval', '12',array('class' => 'form-control'));}}
     	{{Form::select('time_unit', array('D' => 'Day', 'M' => 'Month', 'Y' => 'Year'));}}
-    	</br>
-    	{{Form::submit('Set the Goal!');}} 	
+    	<br>
+    	
+    	{{Form::submit('Set the Goal!', array('class' => 'btn btn-success'));}} 	
+	
 	{{ Form::close() }}
 	</br>
     </div> <!-- /container -->
