597b8929bd08a502dd87cc515ef749aebad37eaa
Yang Zhou <gavinzhou10c@gmail.com>
2014-03-02 23:27:07 -0500

    Add the help page.

--- app/views/help.blade.php
+++ app/views/help.blade.php
@@ -1,4 +1,9 @@
 @extends('layouts.main')
 @section('content')	
-	<h1>Coming soon</h1>	
+	<h2>Registration</h2>
+	<p>If you are using our site for the first time, you have to create a new account.</p>	
+	<p>For creating a new account, click the sign up button on the home page, then enter all required information. Then you can use your email and password to login.</p>
+	<h2>Login</h2>
+	<p>Use the login field to login. You have to enter proper username and password</p>
+	<h2>View Progress</h2>
 @stop
