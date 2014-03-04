d523d3961f2ac44db58b5f4b1742c23431225bed
arthurkam <arthur.kam@hotmail.com>
2014-03-04 16:38:50 -0500

    Fix diary autocomplete to poll server

--- app/views/layouts/main.blade.php
+++ app/views/layouts/main.blade.php
@@ -36,6 +36,8 @@
       ga('create', 'UA-47850364-1', 'inc.gs');
       ga('send', 'pageview');
     </script>
+    <script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
+
   </head>
 
   <body>
@@ -108,7 +110,6 @@
     <!-- Bootstrap core JavaScript
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
-    <script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
     <script src="/js/bootstrap.min.js"></script>
   </body>
 </html>
