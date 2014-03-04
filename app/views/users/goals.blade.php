004eaf5454350402a51df9249b750a0c2a6aa223
Yang Zhou <gavinzhou10c@gmail.com>
2014-03-04 02:40:28 -0500

    Enable the goal adding function,and add two tabs in the goal option.

--- app/views/users/goals.blade.php
+++ app/views/users/goals.blade.php
@@ -1,8 +1,23 @@
 @extends('layouts.main')
-@section('content')	
+@section('content')
  <br>
+
+  <ul class="nav nav-tabs">
+		<li class="active"><a href="#overview" data-toggle="tab">Overview</a></li>
+		<li><a href="#Add" data-toggle="tab">Add new Goals</a></li>
+  </ul>
+
+  <div class="tab-pane fade in active" id="overview">
+  <br>
+  	{{{$id = Auth::user()->id}}}
+  	<br>
+  	{{Form::label('Goals:')}}
+
+  </div>
+
+  <div class="tab-pane fade" id="Add">
 	{{ Form::open(array('url' => 'goals/set')) }}
-    	{{Form::label('Goals', 'Choose your goals: ')}}   
+    	{{Form::label('Goals', 'Choose your goals: ')}}
     	{{Form::select('goal_type', array('L' => 'Lose weight', 'G' => 'Gain weight'));}}
     	<br>
     	{{Form::text('weight', '10', array('class' => 'form-control'));}}
@@ -12,11 +27,11 @@
     	{{Form::text('time_interval', '12',array('class' => 'form-control'));}}
     	{{Form::select('time_unit', array('D' => 'Day', 'M' => 'Month', 'Y' => 'Year'));}}
     	<br>
-    	
-    	{{Form::submit('Set the Goal!', array('class' => 'btn btn-success'));}} 	
-	
+  </div>
+    	{{Form::submit('Set the Goal!', array('class' => 'btn btn-success'));}}
+
 	{{ Form::close() }}
 	</br>
     </div> <!-- /container -->
-    
+
 @stop
