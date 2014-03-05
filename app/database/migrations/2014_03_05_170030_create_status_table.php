2002da99dd48e1238faed80e2a5634ac42f11946
Yang Zhou <gavinzhou10c@gmail.com>
2014-03-05 12:21:48 -0500

    Create the migration file for the status table.

---
+++ app/database/migrations/2014_03_05_170030_create_status_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateStatusTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('status', function($table){
+			$table->integer('id');
+			$table->double('weight');
+			$table->string('weight_unit');
+			$table->double('height');
+			$table->string('height_unit');
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('status');
+	}
+
+}
