c644814f0b79c6ca3d1aa7b083cd02365f08a308
brunoboivin <bruno.boivin@mail.mcgill.ca>
2014-03-05 00:03:35 -0500

    testing select action on autocomplete js

--- app/views/users/diary.blade.php
+++ app/views/users/diary.blade.php
@@ -23,7 +23,7 @@
         $( "#food" ).autocomplete({
           source: "/diary/search",
           select: function( event, ui ) {
-            $( '#to_food_id' ).val( ui.item.id );
+            $( '#food' ).val( ui.item.label );
           }
         });
       });
