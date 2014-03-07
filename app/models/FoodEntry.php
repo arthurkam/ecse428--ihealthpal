16da1b6a0b851a7f3d37d0dfe35d99e0109565f7
brunoboivin <bruno.boivin@mail.mcgill.ca>
2014-03-06 23:26:41 -0500

    added FoodEntry class

---
+++ app/models/FoodEntry.php
@@ -0,0 +1,12 @@
+<?php
+
+//use Illuminate\Auth\UserInterface;
+
+class FoodEntry extends Eloquent{
+	
+	/**
+	 * The database table used by the model.
+	 */
+	protected $table = 'food_entries';
+	
+}
