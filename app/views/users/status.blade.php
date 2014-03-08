3c1afd8f96b5bd70a4697365623ccb6280e93800
Yang Zhou <gavinzhou10c@gmail.com>
2014-03-08 12:13:44 -0500

    Make the status overview to a table layout.

--- app/views/users/status.blade.php
+++ app/views/users/status.blade.php
@@ -2,53 +2,67 @@
 @section('content')
 
 <div class="tab-content">
-  <ul class="nav nav-tabs">
+	<ul class="nav nav-tabs">
 		<li class="active"><a href="#overview" data-toggle="tab">Overview</a></li>
 		<li><a href="#Add" data-toggle="tab">Add New Status</a></li>
-  </ul>
+	</ul>
 
-  <div class="tab-pane fade in active" id="overview">
-  	{{Form::open()}}
-  	<br>
-	  	<div class="text-hide">
-	  	{{$id = Auth::user()->id}}
-	  	</div>
-	  	<br>
-	
-	  	{{Form::label('Your max weight:')}}
-	  	{{Status::where('id',$id)->max('weight');}}
-	  	<br>
-	  	{{Form::label('Your min weight:')}}
-	  	{{Status::where('id',$id)->min('weight');}}
-	  	<br>
-	  	{{Form::label('Your avg weight:')}}
-	  	{{Status::where('id',$id)->avg('weight');}}
-	  	<br>
-	  	{{Form::label('Your max height:')}}
-	  	{{Status::where('id',$id)->max('height');}}
-	  	<br>
-	  	{{Form::label('Your min height:')}}
-	  	{{Status::where('id',$id)->min('height');}}
-	  	<br>
-	  	{{Form::label('Your avg height:')}}
-	  	{{Status::where('id',$id)->avg('height');}}
-	
-  	{{Form::close()}}
-  </div>
-  <div class="tab-pane fade" id="Add">
-  		<br>
-  		{{Form::open(array('url' => 'status/set'))}}
-  		{{Form::label('Update your height:')}}
-  		{{Form::text('height', '10', array('class' => 'form-control'));}}
-  		{{Form::select('height_unit', array('cm' => 'cm', 'inch' =>'inch'));}}
-  		<br>
-  		{{Form::label('Update your weight')}}
-  		{{Form::text('weight', '10', array('class' => 'form-control'));}}
-  		{{Form::select('weight_unit', array('Kg' => 'Kg', 'Lb' =>'Lb'));}}
-  		<br>
-  		<br>
-  		{{Form::submit('Set the Status!', array('class' => 'btn btn-success'));}}
-  		{{Form::close()}}
-  </div>
+	<div class="tab-pane fade in active" id="overview">
+		{{Form::open()}}
+		<br>
+		<div class="text-hide">
+			{{$id = Auth::user()->id}}
+		</div>
+
+		<table class="table">
+			<tr class="success">
+				<td>{{Form::label('Your max weight:')}}</td>
+				<td>{{Status::where('id',$id)->max('weight');}}</td>
+				<td>kg</td>
+			</tr>
+			<tr class="success">
+				<td>{{Form::label('Your min weight:')}}</td>
+				<td>{{Status::where('id',$id)->min('weight');}}</td>
+				<td>kg</td>
+			</tr>
+			<tr class="success">
+				<td>{{Form::label('Your avg weight:')}}</td>
+				<td>{{Status::where('id',$id)->avg('weight');}}</td>
+				<td>kg</td>
+			</tr>
+			<tr class="success">
+				<td>{{Form::label('Your max height:')}}</td>
+				<td>{{Status::where('id',$id)->max('height');}}</td>
+				<td>cm</td>
+			</tr>
+			<tr class="success">
+				<td>{{Form::label('Your min height:')}}</td>
+				<td>{{Status::where('id',$id)->min('height');}}</td>
+				<td>cm</td>
+			</tr>
+			<tr class="success">
+				<td>{{Form::label('Your avg height:')}}</td>			
+				<td>{{Status::where('id',$id)->avg('height');}}</td>
+				<td>cm</td>
+			</tr>
+		</table>
+		{{Form::close()}}
+	</div>
+
+	<div class="tab-pane fade" id="Add">
+		<br>
+		{{Form::open(array('url' => 'status/set'))}}
+		{{Form::label('Update your height:')}}
+		{{Form::text('height', '10', array('class' => 'form-control'));}}
+		{{Form::select('height_unit', array('cm' => 'cm', 'inch' =>'inch'));}}
+		<br>
+		{{Form::label('Update your weight')}}
+		{{Form::text('weight', '10', array('class' => 'form-control'));}}
+		{{Form::select('weight_unit', array('Kg' => 'Kg', 'Lb' =>'Lb'));}}
+		<br>
+		<br>
+		{{Form::submit('Set the Status!', array('class' => 'btn btn-success'));}}
+		{{Form::close()}}
+	</div>
 </div>
 @stop
