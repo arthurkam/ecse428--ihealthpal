0241d95669d531b367ecf6163f6524fb8ebb21b0
arthurkam <arthur.kam@hotmail.com>
2014-03-07 22:15:00 -0500

    Update diary page layout, split in two columns now

--- app/views/users/diary.blade.php
+++ app/views/users/diary.blade.php
@@ -9,41 +9,43 @@
 
 @section('content') 
 
-    <div class="container">
+    <div class="container-fluid">
       <br /><br />
-    <div class="row">
-        {{ Form::open( array('url' => '/diary/add') ) }}
-        {{ Form::hidden( 'id', Input::old( 'to_food_id' ), array( 'id' => 'to_food_id' ) ) }}
-       
-        {{ Form::text( 'food', Input::old( 'food' ), array ('id'=>'food','class' => 'input-lg col-md-10', 'placeholder'=>'Search for a food') ) }}
-        <img class="" id="loading" src="img/ajax.gif" style="display: none;"/>
+      {{ Form::open( array('url' => '/diary/add','role'=>'form') ) }}
+
+      <div class="row">
+        <div class="col-md-6">
+          {{ Form::text( 'food', Input::old( 'food' ), array ('id'=>'food','class' => 'input-lg col-md-12', 'placeholder'=>'Search for a food, or fill it in yourself!') ) }}
+                     <img class="" id="loading" src="img/ajax.gif" style="display: none;"/>
+        </div>
+        <div class="col-md-6">
+          {{ Form::hidden( 'id', Input::old( 'to_food_id' ), array( 'id' => 'to_food_id' ) ) }}
+
+              {{ Form::text( 'name', Input::old( 'name' ), array ( 'class' => 'input-lg', 'placeholder'=>'Name' ) ) }}
+              <br>
+              {{ Form::text( 'quantity', Input::old( 'quantity' ), array ('class' => 'input-lg', 'placeholder'=>'Serving size' ) ) }}
+              {{ Form::text( 'calories', Input::old( 'calories' ), array ('class' => 'input-lg', 'placeholder'=>'Calories' ) ) }}
+               <br />
+              {{ Form::text( 'fat', Input::old( 'fat' ), array ('class' => 'input-lg', 'placeholder'=>'Fat (gr)' ) ) }}
+              {{ Form::text( 'sodium', Input::old( 'sodium' ), array ('class' => 'input-lg', 'placeholder'=>'Sodium (mg)' ) ) }}
+              {{ Form::text( 'carbohydrates', Input::old( 'carbohydrates' ), array ('class' => 'input-lg', 'placeholder'=>'Carbohydrates (gr)' ) ) }}
+              {{ Form::text( 'fibre', Input::old( 'fibre' ), array ('class' => 'input-lg', 'placeholder'=>'Fibre (gr)' ) ) }}
+              {{ Form::text( 'sugars', Input::old( 'sugars' ), array ('class' => 'input-lg', 'placeholder'=>'Sugars (gr)' ) ) }}
+              {{ Form::text( 'protein', Input::old( 'protein' ), array ('class' => 'input-lg', 'placeholder'=>'Protein (gr)' ) ) }}
+              <br/>
+              {{ Form::text( 'vitaminA', Input::old( 'vitaminA' ), array ('class' => 'input-lg', 'placeholder'=>'Vitamin A (daily %)' ) ) }}
+              {{ Form::text( 'vitaminC', Input::old( 'vitaminC' ), array ('class' => 'input-lg', 'placeholder'=>'Vitamin C (daily %)' ) ) }}
+              {{ Form::text( 'calcium', Input::old( 'calcium' ), array ('class' => 'input-lg', 'placeholder'=>'Calcium (daily %)' ) ) }}      
+              {{ Form::text( 'iron', Input::old( 'iron' ), array ('class' => 'input-lg', 'placeholder'=>'Iron (daily %)' ) ) }}
+
+            <img class="" id="loading" src="img/ajax.gif" style="display: none;"/>
+
+            <br/> <br/>
+            {{ Form::submit('Add', array('class'=>'btn btn-lg btn-primary'))}}
+      </div>
+      {{ Form::close() }}
+
     </div>
-    <div class="row">
-          <br />
-          {{ Form::text( 'name', Input::old( 'name' ), array ( 'class' => 'input-lg', 'placeholder'=>'Name' ) ) }}
-          {{ Form::text( 'quantity', Input::old( 'quantity' ), array ('class' => 'input-lg', 'placeholder'=>'Serving size' ) ) }}
-          {{ Form::text( 'calories', Input::old( 'calories' ), array ('class' => 'input-lg', 'placeholder'=>'Calories' ) ) }}
-           <br />
-          {{ Form::text( 'fat', Input::old( 'fat' ), array ('class' => 'input-lg', 'placeholder'=>'Fat (gr)' ) ) }}
-          {{ Form::text( 'sodium', Input::old( 'sodium' ), array ('class' => 'input-lg', 'placeholder'=>'Sodium (mg)' ) ) }}
-          {{ Form::text( 'carbohydrates', Input::old( 'carbohydrates' ), array ('class' => 'input-lg', 'placeholder'=>'Carbohydrates (gr)' ) ) }}
-          {{ Form::text( 'fibre', Input::old( 'fibre' ), array ('class' => 'input-lg', 'placeholder'=>'Fibre (gr)' ) ) }}
-          {{ Form::text( 'sugars', Input::old( 'sugars' ), array ('class' => 'input-lg', 'placeholder'=>'Sugars (gr)' ) ) }}
-          {{ Form::text( 'protein', Input::old( 'protein' ), array ('class' => 'input-lg', 'placeholder'=>'Protein (gr)' ) ) }}
-          <br/>
-          {{ Form::text( 'vitaminA', Input::old( 'vitaminA' ), array ('class' => 'input-lg', 'placeholder'=>'Vitamin A (daily %)' ) ) }}
-          {{ Form::text( 'vitaminC', Input::old( 'vitaminC' ), array ('class' => 'input-lg', 'placeholder'=>'Vitamin C (daily %)' ) ) }}
-          {{ Form::text( 'calcium', Input::old( 'calcium' ), array ('class' => 'input-lg', 'placeholder'=>'Calcium (daily %)' ) ) }}      
-          {{ Form::text( 'iron', Input::old( 'iron' ), array ('class' => 'input-lg', 'placeholder'=>'Iron (daily %)' ) ) }}
-
-        <img class="" id="loading" src="img/ajax.gif" style="display: none;"/>
-
-        <br/> <br/>
-        {{ Form::submit('Add', array('class'=>'btn btn-lg btn-primary'))}}
-        {{ Form::close() }}
-    </div>
-
-
     <br /><br />
 @stop
 @section('code')
