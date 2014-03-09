1202afa9afc1c79884d114a178a689ea11481eac
ChanWoo Yang <chanwoo.yang@mail.mcgill.ca>
2014-03-09 04:35:02 -0400

    Help: Personal Stats tutorial: complete

--- app/views/common/help.blade.php
+++ app/views/common/help.blade.php
@@ -64,7 +64,6 @@
 			      <div class="panel-body">
 			  
 					<!------------- Carousel ----------->
-			
 					<div id="carousel-example-generic1" class="carousel slide" data-ride="carousel">
 					  <!-- Indicators -->
 					  <ol class="carousel-indicators">
@@ -198,7 +197,58 @@
 			    </div>
 			    <div id="collapseThree" class="panel-collapse collapse">
 			      <div class="panel-body">
-			        <p>Coming Soon</p>
+			      
+			        <!------------- Carousel ----------->
+					<div id="carousel-example-generic3" class="carousel slide" data-ride="carousel">
+					  <!-- Indicators -->
+					  <ol class="carousel-indicators">
+					    <li data-target="#carousel-example-generic3" data-slide-to="0" class="active"></li>
+					    <li data-target="#carousel-example-generic3" data-slide-to="1"></li>
+					    <li data-target="#carousel-example-generic3" data-slide-to="2"></li>
+					    <li data-target="#carousel-example-generic3" data-slide-to="3"></li>
+					    <li data-target="#carousel-example-generic3" data-slide-to="4"></li>
+					  </ol>
+					  <!-- Wrapper for slides -->
+					  <div class="carousel-inner">
+					    
+					    <div class="item active">
+					      <img src="/img/stat_tutorial1.png" alt="stat tutorial1">
+					      <div class="carousel-caption">
+							<p style="color:black">Select Diary Icon</p>
+					      </div>
+					    </div>
+					    <div class="item">
+					      <img src="/img/stat_tutorial2.png" alt="stat tutorial2">
+					      <div class="carousel-caption">
+							<p style="color:black">1. Recorded personal body status are displayed</p>
+							<p style="color:black">2. To add new body status, click 'Add New Status' tab</p>
+					      </div>
+					    </div>
+					    <div class="item">
+					      <img src="/img/stat_tutorial3.png" alt="stat tutorial3">
+					      <div class="carousel-caption">
+					      </div>
+					    </div>
+					    <div class="item">
+					      <img src="/img/stat_tutorial4.png" alt="stat tutorial4">
+					      <div class="carousel-caption">
+							<p style="color:black">1. Newly added status is recorded</p>
+							<p style="color:black">2. New body status are calculated and displayed</p>
+					      </div>
+					    </div>
+					    
+					    
+					  </div>					
+					  <!-- Controls -->
+					  <a class="left carousel-control" href="#carousel-example-generic3" data-slide="prev">
+					    <span class="glyphicon glyphicon-chevron-left"></span>
+					  </a>
+					  <a class="right carousel-control" href="#carousel-example-generic3" data-slide="next">
+					    <span class="glyphicon glyphicon-chevron-right"></span>
+					  </a>
+					</div>
+					<!------------- Carousel ends ----------->
+					
 			      </div>
 			    </div>
 			  </div>
