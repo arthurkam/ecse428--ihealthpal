6c96bc6010e5e91dce9e83184ed0f4e8e177da0b
ChanWoo Yang <chanwoo.yang@mail.mcgill.ca>
2014-03-08 23:33:41 -0500

    Help: Goal page tutorial: complete

--- app/views/common/help.blade.php
+++ app/views/common/help.blade.php
@@ -50,6 +50,7 @@
 			<center><img src="/img/instruction2.png" alt="instruction image" height="500"></center>
 
 			<div class="panel-group" id="accordion" >
+			
 			  <div class="panel panel-default">
 			    <div class="panel-heading">
 			      <h4 class="panel-title">
@@ -59,16 +60,19 @@
 			      </h4>
 			    </div>
 			    
-			    <!------------- Carousel ----------->
 			    <div id="collapseOne" class="panel-collapse collapse">
 			      <div class="panel-body">
-					
-					<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+			  
+					<!------------- Carousel ----------->
+			
+					<div id="carousel-example-generic1" class="carousel slide" data-ride="carousel">
 					  <!-- Indicators -->
 					  <ol class="carousel-indicators">
-					    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-					    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
-					    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+					    <li data-target="#carousel-example-generic1" data-slide-to="0" class="active"></li>
+					    <li data-target="#carousel-example-generic1" data-slide-to="1"></li>
+					    <li data-target="#carousel-example-generic1" data-slide-to="2"></li>
+					    <li data-target="#carousel-example-generic1" data-slide-to="3"></li>
+					    <li data-target="#carousel-example-generic1" data-slide-to="4"></li>
 					  </ol>
 					  <!-- Wrapper for slides -->
 					  <div class="carousel-inner">
@@ -109,18 +113,18 @@
 					    
 					  </div>					
 					  <!-- Controls -->
-					  <a class="left carousel-control" href="#carousel-example-generic" data-slide="prev">
+					  <a class="left carousel-control" href="#carousel-example-generic1" data-slide="prev">
 					    <span class="glyphicon glyphicon-chevron-left"></span>
 					  </a>
-					  <a class="right carousel-control" href="#carousel-example-generic" data-slide="next">
+					  <a class="right carousel-control" href="#carousel-example-generic1" data-slide="next">
 					    <span class="glyphicon glyphicon-chevron-right"></span>
 					  </a>
-					</div>			    
-			      </div>
+					</div>
+					<!------------- Carousel ends ----------->
+			    
+			      </div>			       			  
 			    </div>
 			  </div>
- 			  <!------------- Carousel ends ----------->
-
 			  
 			  <div class="panel panel-default">
 			    <div class="panel-heading">
@@ -132,7 +136,55 @@
 			    </div>
 			    <div id="collapseTwo" class="panel-collapse collapse">
 			      <div class="panel-body">
-			        <p>Coming Soon</p>
+			      
+			        <!------------- Carousel ----------->
+					<div id="carousel-example-generic2" class="carousel slide" data-ride="carousel">
+					  <!-- Indicators -->
+					  <ol class="carousel-indicators">
+					    <li data-target="#carousel-example-generic2" data-slide-to="0" class="active"></li>
+					    <li data-target="#carousel-example-generic2" data-slide-to="1"></li>
+					    <li data-target="#carousel-example-generic2" data-slide-to="2"></li>
+					    <li data-target="#carousel-example-generic2" data-slide-to="3"></li>
+					  </ol>
+					  <!-- Wrapper for slides -->
+					  <div class="carousel-inner">
+					    
+					    <div class="item active">
+					      <img src="/img/goal_tutorial1.png" alt="goal tutorial1">
+					      <div class="carousel-caption">
+							<p style="color:black">Select Goal Icon</p>
+					      </div>
+					    </div>
+					    <div class="item">
+					      <img src="/img/goal_tutorial2.png" alt="goal tutorial2">
+					      <div class="carousel-caption">
+							<p style="color:black">1. History of set long-term goals are displayed</p>
+							<p style="color:black">2. To set new goal, click 'Add new goals' tab</p>
+					      </div>
+					    </div>
+					    <div class="item">
+					      <img src="/img/goal_tutorial3.png" alt="goal tutorial3">
+					      <div class="carousel-caption">
+					      </div>
+					    </div>
+					    <div class="item">
+					      <img src="/img/goal_tutorial4.png" alt="goal tutorial4">
+					      <div class="carousel-caption">
+							<p style="color:black">New goal is added to the Goal page</p>
+					      </div>
+					    </div>
+					    					    
+					  </div>					
+					  <!-- Controls -->
+					  <a class="left carousel-control" href="#carousel-example-generic2" data-slide="prev">
+					    <span class="glyphicon glyphicon-chevron-left"></span>
+					  </a>
+					  <a class="right carousel-control" href="#carousel-example-generic2" data-slide="next">
+					    <span class="glyphicon glyphicon-chevron-right"></span>
+					  </a>
+					</div>
+					<!------------- Carousel ends ----------->
+					
 			      </div>
 			    </div>
 			  </div>
