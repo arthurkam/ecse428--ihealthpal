781729c36a055908a9635ee16274f7bb7e498290
arthurkam <arthur.kam@hotmail.com>
2014-03-21 17:32:06 -0400

    Update GoalsController add achievements

--- app/controllers/AchievementsController.php
+++ app/controllers/AchievementsController.php
@@ -1,13 +1,27 @@
 <?php
 
 class AchievementsController extends BaseController {
-
+public function checkAchievements(){
+	$currWeight  = Auth:user()->weight;
+	foreach($achievements as $achievement){
+		//if the achievement hasnt been completed, you check it
+		if($achievement->completed===0 && $achievement->missed===0){
+			$eta = $achievement->eta;
+		}
+	}
+}
 public function showAchievements()
 	{
 		Session::regenerate();
 		
 		if(Auth::check())
 		{
+			$id = Auth::user()->id;
+			//get all the gaols
+			$achievements = Goal::where('uid',$id)->get();
+			foreach($achievements as $achievement){
+				
+			}
 			return View::make('users.achievements');
 		}
 		
---
+++ app/models/Achievement.php
@@ -0,0 +1,12 @@
+<?php
+
+//use Illuminate\Auth\UserInterface;
+
+class Achievement extends Eloquent{
+    
+    /**
+     * The database table used by the model.
+     */
+    protected $table = 'achievements';
+    
+}
