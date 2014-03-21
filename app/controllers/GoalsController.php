433cea2d5bd348a75c36722fa72a9d963799ee3e
arthurkam <arthur.kam@hotmail.com>
2014-03-21 17:32:06 -0400

    Update GoalsController add achievements

--- app/controllers/GoalsController.php
+++ app/controllers/GoalsController.php
@@ -10,7 +10,7 @@ class GoalsController extends BaseController
 		if(Auth::check())
 		{
 			$id = Auth::user()->id;
-			$goals = Goal::where('id', $id)->get();
+			$goals = Goal::where('uid', $id)->get();
   			
   			return View::make('users.goals')->with('goals',$goals);	
 			
@@ -21,25 +21,52 @@ class GoalsController extends BaseController
 			
 	public function setGoals()
 	{
+		$day_len = 86400;
+		$month_len = 2592000;
+		$year_len = 31536000;
+
 		$validator = Validator::make(
 			array(
-				'weight' => Input::get('weight')			
+				'weight' => Input::get('weight'),
+				'time_interval'=>Input::get('time_interval'),			
 			),
 			array(
+				'time_interval'=>'required|integer',
 				'weight' => 'required|integer',
 			)
 		);
 		if(Auth::check() && $validator->passes())
 		{
 			$goal = new Goal;
-			$goal->id = Auth::user()->id;
+			$goal->uid = Auth::user()->id;
 			$goal->goal_type = Input::get('goal_type');
 			$goal->weight = Input::get('weight');
 			$goal->weight_unit = Input::get('weight_unit');
 			$goal->time_interval = Input::get('time_interval');
 			$goal->time_unit = Input::get('time_unit');
 			$goal->save();
-			
+
+			$achievement = new Achievement;
+			$achievement->uid = Auth::user()->id;
+			$weightMultiplier = Input::get("goal_type")==="Lose"?-1:1;
+			//weight is goal weight
+			$achievement->weight = $weightMultiplier*Input::get('weight')+Auth::user()->weight;
+			$achievement->weight_unit = Input::get('weight_unit');
+			$achievement->goal_type = Input::get('goal_type');
+
+			// ugly ternary operator chain, dont copy me.
+			$endDate = ((int)Input::get('time_interval'))*
+				(Input::get('time_unit')==='Days'?$day_len:(
+						Input::get('time_unit')==='Months'?$month_len:
+							$year_len));
+			$endDate+=time();
+			$achievement->start_date = date( 'Y-m-d H:i:s', time());;
+			$achievement->eta = date( 'Y-m-d H:i:s', $endDate);
+
+			$achievement->completed = false;
+			$achievement->missed = false;
+			$achievement->save();
+
 			return Redirect::to('goals');
 		}	
 		else if(Auth::check() && $validator->fails()){
--- app/views/users/achievements.blade.php
+++ app/views/users/achievements.blade.php
@@ -2,5 +2,51 @@
 @section('content')
 
 <h2>Achievements</h2>
+@if(count($totalAchievements)===0)
+Sorry you achievements are empty, please set a goal or something
+@else
+<ul class="nav nav-tabs">
+  <li class="active"><a href="#home" data-toggle="tab">All</a></li>
+  <li><a href="#profile" data-toggle="tab">Completed</a></li>
+  <li><a href="#settings" data-toggle="tab">In Progress</a></li>
+</ul>
+
+<div class="tab-content">
+  <div class="tab-pane active" id="all">
+    @foreach ($achievement as $totalAchievements)
+
+    @endforeach
+  </div>
+  <div class="tab-pane" id="completed">
+    @foreach ($achievement as $completedAchievements)
+
+    @endforeach
+  </div>
+  <div class="tab-pane" id="inProgress">
+    <table class="table table-striped table-hover">
+        <tr>
+            <th>
+                Goal
+            </th>
+            <th>
+                Date
+            </th>
+        </tr>
+    @foreach ($achievement as $inProgressAchievements)
+        <tr>
+            <td>
+                {{goal}}
+            </td>
+            <td>
+                {{date}}
+            </td>
+        </tr>
+    @endforeach
+    </table>
+  </div>
+</div>
+@endif
+@stop
+@section('code')
 
 @stop
