0a355f079fa64fd175ac47dd51d2028c6694b597
arthurkam <arthur.kam@hotmail.com>
2014-03-23 12:21:57 -0400

    made meal plans more modular

--- app/controllers/mealPlanController.php
+++ app/controllers/mealPlanController.php
@@ -8,7 +8,21 @@ public function showMealPlan()
 		
 		if(Auth::check())
 		{
-			return View::make('users.mealPlan');
+			$id = Auth::user()->id;
+
+			$goals = Goal::where('uid',$id)->get();
+			$lastGoal= null;
+			$latestStart = 0;
+			foreach($goals as $goal){
+			        if(strtotime($goal->created_at)>$latestStart){
+			            $latestStart = strtotime($goal->created_at);
+			            $lastGoal = $goal;
+			        }
+			    }
+			
+			$ret = array('lastGoal'=>$lastGoal,
+				'goals'=>$goals);
+			return View::make('users.mealPlan',$ret);
 		}
 		
 		return Redirect::to('/')->with('message', 'Please log in first!');
