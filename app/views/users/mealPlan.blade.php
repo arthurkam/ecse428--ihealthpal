4ea8e7ff924f525326f2965719a8cd54bb66164d
ChanWoo Yang <chanwoo.yang@mail.mcgill.ca>
2014-03-23 02:56:13 -0400

    meal plan: mass gain 2

--- app/views/users/mealPlan.blade.php
+++ app/views/users/mealPlan.blade.php
@@ -202,7 +202,241 @@
 <! http://www.bodybuilding.com/fun/mohr60.htm#1 >  
   <div class="tab-pane active" id="massGain2">
     <div class="row">
-    ...
+    
+    <div class="panel panel-default">
+	  <!-- Default panel contents -->
+	  <div class="panel-heading"><font size="5"><b>Mass Gain Meal Plan 2</b></font></div>
+	  <div class="panel-body">
+	    <p><font size="3">Eating 4000 calories each day may make you feel like a bear that is getting ready to hibernate during the winter, but when you build serious muscle than when the small amount of fat storage that comes along with gaining extreme muscle can be hidden underneath your winter clothes. Keep your eye on that mirror; there's no better way to monitor your gains.</font></p>
+	  </div>
+	
+	  <!-- Table -->
+	  <table class="table">
+	    <tr>
+	      <td width="50">     </td>
+		  <td width="100"><b>Day 1</b></td>
+		  <td width="100"><b>Day 2</b></td> 
+		  <td width="100"><b>Day 3</b></td>
+		  <td width="100"><b>Day 4</b></td>
+		  <td width="100"><b>Day 5</b></td>
+		  <td width="100"><b>Day 6</b></td>
+		  <td width="100"><b>Day 7</b></td>
+		</tr>
+		<tr>
+		  <td><b>Breakfast</b></td>
+		  <td>
+		  <li>1.5 Cups Raw Oatmeal</li>
+		  <li>1 Cup Skimmed Milk</li>
+		  <li>1/2 Cup Dried Cranberries or Raisins</li>
+		  <li>1TBS Flax Oil</li>
+		  </td>
+		  <td>
+		  <li>4 Whole Grain Frozen Waffles</li>
+		  <li>2 TBS Pure Maple Syrup</li>
+		  <li>1 Cup Low-fat Cottage Cheese</li>
+		  <li>1 Cup Fresh Strawberries</li>
+		  </td>
+		  <td>
+		  <li>Vegetable Omelet</li>
+		  <li>2 Slices 12-Grain Bread</li>
+		  <li>1 Cup Non-fat Milk</li>
+		  <li>1 Fresh Orange</li>
+		  </td>
+		  <td>
+		  <li>Oat Bran Waffles</li>
+		  <li>1 Cup Skimmed Milk</li>
+		  </td>
+		  <td>
+		  <li>Buckwheat Pancake</li>
+		  <li>2 Cups of Skimmed Milk</li>
+		  <li>1 Cup Fresh Blueberries</li>
+		  </td>
+		  <td>
+		  <li>Breakfast Burritos</li>
+		  <li>1 Medium Grapefruit</li>
+		  </td>
+		  <td>
+		  <li>1 Multi-Grain Bagel, Topped with 2 TBS Reduced Cream Cheese and 4oz Smoked Salmon</li>
+		  <li>1 Pear</li>
+		  </td>
+		</tr>
+		<tr>
+		  <td><b>Midmorning Snack</b></td>
+		  <td>
+		  <li>1 Cup Skimmed Milk</li>
+		  <li>1 Large Piece of Fruit with 1 TBS natural peanut Butter</li>
+		  <li>1 Low-fat Mozzarella Stick</li>
+		  </td>
+		  <td>
+		  <li>Peanut Butter, Banana, and Honey Sandwich</li>
+		  <li>2 Pieces Whole Grain Bread</li>
+		  <li>2 TBS Banana</li>
+		  <li>1 Whole Banana</li>
+		  <li>2 TBS Honey</li>
+		  <li>2 Cups Non-fat Milk</li>
+		  </td>
+		  <td>
+		  <li>1 Can Tuna fish</li>
+		  <li>2 Cups cooked Brown Rice</li>
+		  <li>Salsa to taste</li>
+		  <li>1 Medium Plum</li>
+		  </td>
+		  <td>
+		  <li>Protein Shake</li>
+		  <li>1 Cup Skimmed Milk</li>
+		  <li>2 Large Pieces of Fruit</li>
+		  </td>
+		  <td>
+		  <li>Protein Shake</li>
+		  <li>1 Cup Skimmed Milk</li>
+		  <li>2 Large Pieces of Fruit</li>
+		  </td>
+		  <td>
+		  <li>2 Cups Oat Bran</li>
+		  </td>
+		  <td>
+		  <li>Whole Grain, hydrogenated Oil Free Crackers</li>
+		  <li>1 Cup Low-fat Cottage Cheese</li>
+		  <li>1 Orange</li>
+		  </td>
+		</tr>
+		<tr>
+		  <td><b>Lunch</b></td>
+		  <td>
+		  <li>2 Cups Egg Salad on 2 Whole Wheat Pitas</li>
+		  <li>1 Banana</li>
+		  </td>
+		  <td>
+		  <li>Pasta Vegetable Medley</li>
+		  <li>1 Cup Non-fat Milk</li>
+		  <li>Spinach</li>
+		  </td>
+		  <td>
+		  <li>Turkey and Cheese Sandwich</li>
+		  <li>1 Small Bunch Red Grapes</li>
+		  </td>
+		  <td>
+		  <li>Tuna and Cheese Sandwich</li>
+		  <li>1 Large Pear</li>
+		  </td>
+		  <td>
+		  <li>Ham and Cheese Sandwich</li>
+		  <li>1 Large Apple</li>
+		  </td>
+		  <td>
+		  <li>2 Frozen Organic Burrito</li>
+		  <li>Carrot Sticks</li>
+		  </td>
+		  <td>
+		  <li>2 Slices whole Grain Bread with 6oz Ham, 2oz Reduced Fat Cheese, sliced Tomato and Lettuce, and Honey Mustards</li>
+		  <li>Veggie Sticks</li>
+		  <li>1 Cup Low-fat Milk</li>
+		  </td>
+		</tr>
+		<tr>
+		  <td><b>Afternoon Snack</b></td>
+		  <td>
+		  <li>1 Cup Low-fat Vanilla Yogurt</li>
+		  <li>1 Cup Fat-free Cottage Cheese</li>
+		  <li>1 Cup Blueberries</li>
+		  <li>2 TBS Wheat Germ Oil</li>
+		  <li>1 TBS Honey</li>
+		  </td>
+		  <td>
+		  <li>Protein Shake with 1 cup Non-fat Milk, 1 Cup Frozen Fruits, and 2 TBS Flax Oil (Add water to desired consistency)</li>
+		  </td>
+		  <td>
+		  <li>Favorite Meal Replacement Powder</li>
+		  <li>1 Cup Skimmed Milk</li>
+		  <li>2 Cups Frozen Fruit</li>
+		  <li>(Add water to desired consistency)</li>
+		  </td>
+		  <td>
+		  <li>Protein Shake</li>
+		  <li>1 Cup Skimmed Milk</li>
+		  <li>2 Large Pieces of Fruit</li>
+		  </td>
+		  <td>
+		  <li>Protein Shake</li>
+		  <li>1 Cup Skimmed Milk</li>
+		  <li>2 Large Pieces of Fruit</li>
+		  </td>
+		  <td>
+		  <li>Triple Decker Peanut Butter and Banana</li>
+		  <li>1 Cup Organic Black Bean Soup</li>
+		  </td>
+		  <td>
+		  <li>Favorite Meal Replacement Powder with 2 Cups Low-fat Milk, 2 Cups Frozen Fruit, and 1 TBS Flax Oil (Add water to desire thickness)</li>
+		  </td>
+		</tr>
+		<tr>
+		  <td><b>Dinner</b></td>
+		  <td>
+		  <li>6oz Grilled Salmon</li>
+		  <li>1 Large Sweet Potato</li>
+		  <li>1 Cup Cut Green Beans</li>
+		  <li>1 Cup Skimmed Milk</li>
+		  </td>
+		  <td>
+		  <li>8oz Turkey Breast</li>
+		  <li>2 Large Sweet Potatoes</li>
+		  <li>1 Cup Collard Greens or Swiss Chard</li>
+		  </td>
+		  <td>
+		  <li>16oz Grilled Pork Chop</li>
+		  <li>1 Cup Broccoli</li>
+		  <li>2 Cups Whole Grain, Brown Rice</li>
+		  </td>
+		  <td>
+		  <li>1 Bowl Mexican Chicken Chill</li>
+		  </td>
+		  <td>
+		  <li>1 Homemade Burger on Whole Wheat Bun</li>
+		  <li>1 Cup Skimmed Milk</li>
+		  </td>
+		  <td>
+		  <li>6oz Tuna Steak</li>
+		  <li>1 Cup Whole Wheat Pastas</li>
+		  <li>1 Cup Low-fat Milk</li>
+		  </td>
+		  <td>
+		  <li>1 Homemade 6oz Sirloin Burger on Whole Wheat Bun</li>
+		  <li>2 Cups Steamed Mixed Vegetables</li>
+		  <li>1 Large Sweet Potato</li>
+		  <li>1 Cup Skimmed Milk</li>
+		  </td>
+		</tr>
+		<tr>
+		  <td><b>After Dinner Snack</b></td>
+		  <td>
+		  <li>Peanut Butter Smoothie</li>
+		  </td>
+		  <td>
+		  <li>High Protein Pudding</li>
+		  </td>
+		  <td>
+		  <li>Protein Shake</li>
+		  <li>1 Large Piece of Fruit</li>
+		  </td>
+		  <td>
+		  <li>Protein Shake</li>
+		  <li>1 Large Piece of Fruit</li>
+		  </td>
+		  <td>
+		  <li>Protein Shake</li>
+		  <li>1 Large Piece of Fruit</li>
+		  </td>
+		  <td>
+		  <li>Favorite Meal Replacement Powder with 2 Cups Low-fat Milk, 2 Cups Frozen Fruit, and 1 TBS Flax Oil (Add water to desire thickness)</li>
+		  </td>
+		  <td>
+		  <li>1 Cup Low-fat Cottage Cheese with 1 Cup Mixed Fruit Canned in own Juice</li>
+		  </td>
+		</tr>
+	  </table>
+	</div>
+
+    
 	</div>
   </div>
   
