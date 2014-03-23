fec06eaeaed0b074df6dbce77af3989df6f1e557
ChanWoo Yang <chanwoo.yang@mail.mcgill.ca>
2014-03-23 00:15:21 -0400

    Meal Plan: Gain Mass 1

--- app/views/users/mealPlan.blade.php
+++ app/views/users/mealPlan.blade.php
@@ -14,12 +14,192 @@
 <!-- Tab panes -->
 <div class="tab-content">
 
+<! http://www.menshealth.co.uk/food-nutrition/muscle-foods/eat-all-week-to-build-muscle-332752 >
   <div class="tab-pane active" id="massGain1">
     <div class="row">
-    ...
+    
+    <div class="panel panel-default">
+	  <!-- Default panel contents -->
+	  <div class="panel-heading"><font size="5"><b>Mass Gain Meal Plan 1</b></font></div>
+	  <div class="panel-body">
+	    <p><font size="3">After a workout your body cries out for additional sustenance to repair the damage you’ve done to your muscles but trying to finance this with the standard three meals a day won’t yield any results. Here’s a complete muscle-building eating plan from sports nutritionist.</font></p>
+	  </div>
+	
+	  <!-- Table -->
+	  <table class="table">
+	    <tr>
+	      <td width="50">     </td>
+		  <td width="100"><b>Day 1</b></td>
+		  <td width="100"><b>Day 2</b></td> 
+		  <td width="100"><b>Day 3</b></td>
+		  <td width="100"><b>Day 4</b></td>
+		  <td width="100"><b>Day 5</b></td>
+		  <td width="100"><b>Day 6</b></td>
+		  <td width="100"><b>Day 7</b></td>
+		</tr>
+		<tr>
+		  <td><b>Breakfast</b></td>
+		  <td>
+		  <li>3-egg omelette with:<br>Sliced Mushrooms,<br>Olive Oil,<br>Diced Onions,<br>Diced Tomatoes</li>
+		  </td>
+		  <td>
+		  <li>Handful of Oats</li>
+		  <li>250ml Skimmed Milk</li>
+		  <li>10ml Honey</li>
+		  <li>Whey Protein</li>
+		  <li>Cook and mix together</li></td> 
+		  <td>
+		  <li>2 Scrambled Eggs</li>
+		  <li>1 Can of Baked Beans</li>
+		  <li>2 Slices of Toast (Rye Bread)</li>
+		  <li>200ml Orange Juice</li>
+		  </td>
+		  <td>
+		  <li>Smoothies made from:<br>Whey concentrate powder<br>Handful of Blueberries<br>Handful of Raspberries<br>40ml Plain Yogurt<br>1tbsp Rolled Oats<br>1 Banana</li>
+		  </td>
+		  <td>
+		  <li>4 Weetabix with:<br>250ml Skimmed Milk<br>Fruit Yogurt<br>Handful of Blackberries</li>
+		  </td>
+		  <td>
+		  <li>Smoothies made from:<br>Whey Protein<br>1 Ripe Banana<br>300ml Water<br>Pinch of Ground Cinnamon<br>20g Ground Almonds</li>
+		  </td>
+		  <td>
+		  <li>3 Poached Eggs</li>
+		  <li>Low-fat Sausages</li>
+		  <li>2 Slices of Toast</li>
+		  <li>1 Tomato</li>
+		  </td>
+		</tr>
+		<tr>
+		  <td><b>Midmorning Snack</b></td>
+		  <td>
+		  <li>150ml Plain Yogurt with:<br>Blueberries<br>Drizzle with Manuka honey<br>Ground mixed seeds and nuts</li>
+		  </td>
+		  <td>
+		  <li>2 Boiled Eggs</li>
+		  <li>1 Banana</li>
+		  </td>
+		  <td>
+		  <li>1 Tine of Sardines on Oat Crackers</li>
+		  <li>1 Pear</li>
+		  </td>
+		  <td>
+		  <li>2 Boiled Eggs</li>
+		  <li>1 Apple</li>
+		  </td>
+		  <td>
+		  <li>1 Avocado Topped with:<br>Handful of King Prawns<br>Sliced Mango<br>Drizzle with Lemon Juice</li>
+		  </td>
+		  <td>
+		  <li>1 Toasted Bagel with:<br>1 Mashed Banana<br>Peanut Butter<br>Manuka Honey</li>
+		  </td>
+		  <td>
+		  <li>Toasted Cheese and Tomato on<br>2 Slices of Rye Bread</li>
+		  <li>1 Apple</li>
+		  </td>
+		</tr>
+		<tr>
+		  <td><b>Lunch</b></td>
+		  <td>
+		  <li>Chicken Breast</li>
+		  <li>Avocado</li>
+		  <li>Tomato</li>
+		  <li>Cheese</li>
+		  <li>Baguette</li>
+		  </td>
+		  <td>
+		  <li>300g Fillet Steak</li>
+		  <li>Mashed Potato</li>
+		  <li>Spinach</li>
+		  </td>
+		  <td>
+		  <li>Small Jacket Potato with:<br>Baked Beans<br>Cheese<br>Sliced Pork Sausages<br>Chillies</li>
+		  </td>
+		  <td>
+		  <li>2 Turkey Breast Fillets</li>
+		  <li>Cranberry</li>
+		  <li>Cheese</li>
+		  <li>Rocket Wraps</li>
+		  </td>
+		  <td>
+		  <li>Small Jacket Potato with:<br>Tuna<br>Sweetcorn<br>Low-fat Mayo<br>Chillies<br>Cheese</li>
+		  <li>1 Apple</li>
+		  </td>
+		  <td>
+		  <li>Peppered Mackerel</li>
+		  <li>Avocado</li>
+		  <li>Mixed Salad Baguette</li>
+		  </td>
+		  <td>
+		  <li>Mixed Sushi Box</li>
+		  </td>
+		</tr>
+		<tr>
+		  <td><b>Afternoon Snack</b></td>
+		  <td>
+		  <li>Protein bar</li>
+		  <li>1 Banana</li>
+		  </td>
+		  <td>
+		  <li>Mixed Seeds, Dried Fruits and Nuts</li>
+		  <li>200ml of cottage cheese</li>
+		  </td>
+		  <td>
+		  <li>Plain Sugar-Free Yogurt with:<br>Sliced Banana and Strawberries<br>Ground Mixed Nuts and Seeds</li>
+		  </td>
+		  <td>
+		  <li>150g of cottage cheese</li>
+		  <li>Diced Pineapple on Rice or Oat Cakes</li>
+		  </td>
+		  <td>
+		  <li>1 Grilled Seasoned Chicken Breast with 2 small Beetroots</li>
+		  </td>
+		  <td>
+		  <li>2 Boiled Eggs</li>
+		  <li>2 Slices of Rye Bread</li>
+		  <li>1 Apple</li>
+		  </td>
+		  <td>
+		  <li>Oatcakes with:<br>1 Avocado<br>Sliced Proscuitto Ham<br>Salsa</li>
+		  </td>
+		</tr>
+		<tr>
+		  <td><b>Dinner</b></td>
+		  <td>
+		  <li>Tuna Steak with:<br>Stir-fried mixed veggies in olive oil<br>Soba noodles</li>
+		  </td>
+		  <td>
+		  <li>Chicken and Prawn Curry with Soba Noodles</li>
+		  </td>
+		  <td>
+		  <li>Lean Minced Beef Chilli with Mixed Veggies, Beans and Basmati Rice</li>
+		  </td>
+		  <td>
+		  <li>Lemon Sole Fillet</li>
+		  <li>2 Handfuls of Baby Potatoes</li>
+		  <li>Mixed Vegetables</li>
+		  </td>
+		  <td>
+		  <li>4 Lamb Chops</li>
+		  <li>100g Boiled Potatoes</li>
+		  <li>Mixed Vegetables</li>
+		  </td>
+		  <td>
+		  <li>2 Grilled Chicken Breasts Seasoned with Cajun Pepper</li>
+		  <li>1 Cup of Couscous</li>
+		  <li>Mixed Roasted Vegetables</li>
+		  </td>
+		  <td>
+		  <li>2 Grilled Chicken Breasts with:<br>Diced Radishes<br>Green Salad<br>Couscous<br>Dress with Olive Oil and Balsamic Vinegar</li>
+		  </td>
+		</tr>
+	  </table>
+	</div>
+
 	</div>
   </div>
-  
+
+<! http://www.bodybuilding.com/fun/mohr60.htm#1 >  
   <div class="tab-pane active" id="massGain2">
     <div class="row">
     ...
