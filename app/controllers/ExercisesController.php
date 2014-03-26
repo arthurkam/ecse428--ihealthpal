af6811493ef2a305e969a73f12e473bfd7d0e340
Yang Zhou <gavinzhou10c@gmail.com>
2014-03-26 10:30:11 -0400

    Add Validator to the exercise

--- app/controllers/ExercisesController.php
+++ app/controllers/ExercisesController.php
@@ -19,15 +19,32 @@ class ExercisesController extends BaseController {
 	public function saveExercises()
 	{
 		if(Auth::check()){
-			$id = Auth::user()->id;
-			$exercise = new Exercise;
-			$exercise->uid = $id;
-			$exercise->exercise = Input::get('exercise');
-			$exercise->weight = Input::get('weight');
-			$exercise->sets = Input::get('sets');
-			$exercise->reps = Input::get('reps');
-			$exercise->save();
-			return Redirect::to('exercises')->with('message', 'Your exercise has been saved!');
+		
+			$validator = Validator::make(
+				array(
+					'weight'=> Input::get('weight'),
+					'sets' => Input::get('sets'),
+					'reps' => Input::get('reps')
+				),	
+				
+				array(
+					'weight' =>'required|Integer|min:0',
+					'sets' => 'required|Integer|min:1',
+					'reps' => 'required|Integer|min:1'
+				)
+			);
+			if($validator -> passes()){
+				$id = Auth::user()->id;
+				$exercise = new Exercise;
+				$exercise->uid = $id;
+				$exercise->exercise = Input::get('exercise');
+				$exercise->weight = Input::get('weight');
+				$exercise->sets = Input::get('sets');
+				$exercise->reps = Input::get('reps');
+				$exercise->save();
+				return Redirect::to('exercises')->with('message', 'Your exercise has been saved!');
+			}	
+			return Redirect::to('exercises')->with('message', 'The following errors occurred')->withErrors($validator)->withInput();
 		}
 		return Redirect::to('/')->with('message', 'Please log in first!');
 	}
